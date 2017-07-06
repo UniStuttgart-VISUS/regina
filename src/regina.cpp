8124f7dc4afe9ebdc0dd246ea0473c7739eda01a
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2017-07-06 18:48:15 +0200

    Started implementing new instrumentation method

--- src/regina.cpp
+++ src/regina.cpp
@@ -23,9 +23,11 @@ typedef struct _threadData_t {
 
 typedef struct _traceData_t {
     void *iAddr;
+    void *dAddr;
     int32_t dSize;
     int32_t iType;
-    int64_t cCycle;
+    uint64_t startT;
+    uint64_t endT;
 } traceData_t;
 
 #define MAX_NUM_MEM_REFS 1000000
@@ -37,13 +39,21 @@ static void instrument(void *drcontext, instrlist_t *ilist, instr_t *where,
     int pos, bool write);
 
 
-static void clean_call(void) {
+/**
+ * DR clean call that writes current trace buffer onto disk.
+ * Should only be called when trace buffer is about to overflow.
+ */
+static void ccTraceIO(void) {
     void *drcontext = dr_get_current_drcontext();
 
     //memtrace(drcontext);
 }
 
 
+/**
+ * DR clean call to insert performance counter to get real-time stamp.
+ * This call is very expensive, but should be free of system_calls.
+ */
 static void ccQueryPerformanceCounter(void) {
     LARGE_INTEGER timer;
     QueryPerformanceCounter(&timer);
@@ -65,7 +75,7 @@ static void code_cache_init(void) {
 
     where = INSTR_CREATE_jmp_ind(drcontext, opnd_create_reg(DR_REG_XCX));
     instrlist_meta_append(ilist, where);
-    dr_insert_clean_call(drcontext, ilist, where, (void *)clean_call,
+    dr_insert_clean_call(drcontext, ilist, where, (void *)ccTraceIO,
         false, 0);
 
     end = instrlist_encode(drcontext, ilist, codeCache, false);
@@ -215,8 +225,57 @@ void dr_client_main(client_id_t id, int argc, const char *argv[]) {
     code_cache_init();
 }
 
+/*
+typedef struct _threadData_t {
+FILE *file;
+void *bufferBase;
+void *bufferPtr;
+} threadData_t;
+
+typedef struct _traceData_t {
+    void *iAddr;
+    void *dAddr;
+    int32_t dSize;
+    int32_t iType;
+    uint64_t startT;
+    uint64_t endT;
+} traceData_t;
+*/
 
 static void instrument(void *drcontext, instrlist_t *ilist, instr_t *where,
     int pos, bool write) {
+    reg_id_t reg1;
+    reg_id_t regCX;
 
+    // set vector to reserve some registers, default none
+    drvector_t allowed;
+    drreg_init_and_fill_vector(&allowed, false);
+    // we want to reserve CX register
+    drreg_set_vector_entry(&allowed, DR_REG_XCX, true);
+    // reserve two registers
+    if (drreg_reserve_register(drcontext, ilist, where, &allowed, &regCX) != DRREG_SUCCESS ||
+        drreg_reserve_register(drcontext, ilist, where, nullptr, &reg1) != DRREG_SUCCESS) {
+        drvector_delete(&allowed);
+        return;
+    }
+    drvector_delete(&allowed);
+
+    // specify instructions inserted before where
+    // this would the capturing of meta information on the mem reference
+    // and a perf query to get the start timer
+
+    opnd_t opnd1, opnd2;
+    instr_t *instr;
+
+    // get trace buffer current ptr
+    drmgr_insert_read_tls_field(drcontext, tls_index, ilist, where, regCX);
+    // now load it
+    opnd1 = opnd_create_reg(regCX);
+    opnd2 = OPND_CREATE_MEMPTR(regCX, offsetof(threadData_t, bufferPtr));
+    instr = INSTR_CREATE_mov_ld(drcontext, opnd1, opnd2);
+    instrlist_meta_preinsert(ilist, where, instr);
+
+    // specify instructions inserted after where
+    // this would be a perf query for the end timer
+    // and the overflow check for the trace buffer
 }
